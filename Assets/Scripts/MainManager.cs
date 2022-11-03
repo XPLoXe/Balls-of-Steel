cfd8245973c3a686ad5476d9328ff4df8f8c10eb
Alejandro Bocchicchio <aleb2800@gmail.com>
2022-11-03 18:03:35 -0300

    Player Data Persistance

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using Unity.VisualScripting;
 
 public class MainManager : MonoBehaviour
 {
@@ -9,10 +10,19 @@ public class MainManager : MonoBehaviour
     private AudioSource mainManagerAudioSource;
 
 
-
+    //wave\\
     public int wave;
+    //gems\\
     private int totalGems;
+    //player\\
+    private float Speed { get; set; }
+    private float Mass { get; set; }
+    private float PowerupForce { get; set; }
 
+    public float Strength { get; set; }
+   
+
+    //game\\
     public int difficulty;
     
 
@@ -51,16 +61,12 @@ public class MainManager : MonoBehaviour
         
     }
 
-    public void setTotalGems(int amount)
-    {
-        totalGems += amount;
-    }
+    
 
-    public int getTotalGems()
-    {
-        return totalGems;
-    }
+   
 
+
+    //WAVE DATA\\
     class SaveData
     {
         public int wave;
@@ -71,6 +77,35 @@ public class MainManager : MonoBehaviour
         
     }
 
+    public void SaveWave(int waveToSave)
+    {
+        SaveData data = new SaveData(waveToSave);
+        //data.wave = wave;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/highWave.json", json);
+
+    }
+
+    public int LoadWave()
+    {
+        string path = Application.persistentDataPath + "/highWave.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            //wave = data.wave;
+
+            return data.wave;
+        }
+
+        return 0;
+    }
+
+
+    //GEM DATA\\
     class GemData
     {
         public int gems;
@@ -80,6 +115,7 @@ public class MainManager : MonoBehaviour
             this.gems = gems;
         }
     }
+
 
     public void SaveGems()
     {
@@ -103,31 +139,55 @@ public class MainManager : MonoBehaviour
         return 0;
     }
 
-    public void SaveWave(int waveToSave)
+    public void setTotalGems(int amount)
     {
-        SaveData data = new SaveData(waveToSave);
-        //data.wave = wave;
-
-        string json = JsonUtility.ToJson(data);
-        File.WriteAllText(Application.persistentDataPath + "/highWave.json", json);
-        
+        totalGems += amount;
     }
 
-    public int LoadWave()
+    public int getTotalGems()
     {
-        string path = Application.persistentDataPath + "/highWave.json";
+        return totalGems;
+    }
+
+    //PLAYER DATA\\
+    class PlayerData
+    {
+        public float speed;
+        public float mass;
+        public float powerupForce;
+        public float strength;
+
+        public PlayerData(float speed, float mass, float powerupForce, float strength)
+        {
+            this.speed = speed;
+            this.mass = mass;
+            this.powerupForce = powerupForce;
+            this.strength = strength;
+        }
+    }
+
+    public void SavePlayer(float speed, float mass, float powerupForce, float strength)
+    {
+        PlayerData data = new PlayerData(speed, mass, powerupForce, strength);
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/player.json", json);
+
+    }
+
+    public void LoadPLayer()
+    {
+        string path = Application.persistentDataPath + "/player.json";
 
         if (File.Exists(path))
         {
             string json = File.ReadAllText(path);
-            SaveData data = JsonUtility.FromJson<SaveData>(json);
-
-            //wave = data.wave;
-
-            return data.wave;
+            PlayerData data = JsonUtility.FromJson<PlayerData>(json);
+            this.Speed = data.speed;
+            this.PowerupForce = data.powerupForce;
+            this.Strength = data.strength;
+            this.Mass = data.mass;
+            //return data;
         }
-
-        return 0;
     }
 
     // Update is called once per frame
