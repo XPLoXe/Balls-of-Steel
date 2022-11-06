0668c4af1b322f8540959d12dfb69af5b6b3465e
Alejandro Bocchicchio <aleb2800@gmail.com>
2022-11-06 13:06:56 -0300

    Show FPS

---
+++ Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FPSCounter : MonoBehaviour
+{
+    public TextMeshProUGUI fpsText;
+    public float deltaTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        fpsText.text = Mathf.Ceil (fps).ToString ();
+    }
+}
